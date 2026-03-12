2c5ae32c90b45e8e4848cda0031dc73bb9f39a0d
celine <chungceline7@gmail.com>
2026-03-11 20:44:28 -0400

    Initial Commit

---
+++ terrace/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  terrace
+//
+//  Created by celine on 2026-03-11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
